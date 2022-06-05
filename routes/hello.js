bd1cc071229bd97b9b381b8e2433f4463f34ce4b
Matt Stubenhofer <matt.stubenhofer@gmail.com>
2022-06-05 10:04:43 -0500

    removed console logs

--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    console.log('Route - /API/HELLO')
-    res.json({greeting: 'hello API'})
-})
+router.get("/", (req, res) => { res.json({greeting: 'hello API'}) })
 
 module.exports = router
