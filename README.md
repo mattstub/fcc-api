7d60a5b36aba2b6697ffa08eb825ca37a0357853
Matt Stubenhofer <matt.stubenhofer@gmail.com>
2022-06-06 06:13:07 -0500

    v0.2.3 added the valid-url package for short.js

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ freeCodeCamp API Challenges
 - v0.1.1 - *MDS - 06/05/2022* [^2]
 - v0.2.1 - *MDS - 06/05/2022* [^3]
 - v0.2.2 - *MDS - 06/06/2022* [^4]
+- v0.2.3 - *MDS - 06/06/2022* [^5]
 
 [^1]: Timestamp Microservice fully functioning locally and remotely as an independent project on Heroku, 6/2/2022. Passing all freeCodeCamp conditions for Timestamp Microservice
 [^2]: Moved Timestamp Microservice into a routed setup so that all APIs can be written in the same project. Working Locally and passing codes remotely.
 [^3]: Request Header Parser Microservice functioning locally and remotely. Timestamp Microservice also still passing all conditions.
 [^4]: Revised URL Shortener Microservice to asynchronous. Also added in ability to check and see if URL is already in system, and return its value if so instead of putting another iteration in the system causing the system to fail on subsequent attempts. All conditions are working locally again, and hopefully with asynchronous feature there will be enough of a catch to not crash the heroku app out. Also added a timeout feature to the database connection so that it would timeout after 5 seconds instead of default 30 seconds. Hoping this will also keep the app from crashing.
+[^5]: Revised URL again to use the 'valid-url' package instead of a regex statement. Hopefully this will help us pass fcc conditions.
 
 ### API Links
 - **Timestamp Microservice:** [Heroku](https://fcc-ms-api.herokuapp.com/api/timestamp/landing) | [Instructions](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice) | [Conditions](/documentation/01-timestamp.md)
--- routes/short.js
+++ routes/short.js
@@ -8,17 +8,11 @@ const router = express.Router()
 const path = require('path')
 const bodyParser = require('body-parser')
 const shortid = require('shortid')
+const validURL = require('valid-url')
 const ShortURL = require('../models/suffix.js')
 
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
-
-// Using Regex, validates the url to insure its a working url
-// Regex from https://gist.github.com/dperini/729294
-function validateURL(url) {
-    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-    return regex.test(url);
-}
 
 // Route - Index Page for Information Purposes
 router.get("/landing", (req, res) => {
@@ -33,7 +27,7 @@ router.post('/', async (req, res) => {
     const suffix = shortid.generate()
     
     // If URL is not valid, an error JSON object will be displayed, matching criteria
-    if(!validateURL(client_url)) {
+    if(!validURL.isUri(client_url)) {
         res.status(401).json({ error: 'invalid url' })
     // Else If the URL is valid, Search for Existing
     } else {
@@ -69,10 +63,20 @@ router.post('/', async (req, res) => {
 // Router - GET API Endpoint
 // Locate the correct shortened URL from the database
 // Once located, redirect the webpage to the original_url
-router.get('/:shorturl', (req, res) => {
-    ShortURL.find({shorturl: req.params.shorturl}).then(foundURLs => {
-        res.redirect(foundURLs[0].original_url)
-    })
+router.get('/:shorturl', async (req, res) => {
+    try {
+        const foundURL = await ShortURL.findOne({
+            short_url: req.params.shorturl
+        })
+        if(foundURL) {
+            return res.redirect(foundURL.original_url)
+        } else {
+            return res.status(404).json('No URL Found')
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).json('Server error...')
+    }
 })
 
 module.exports = router
